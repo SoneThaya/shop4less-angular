4fbcb60756701817ce1436e08b8c2ae43273cd64
Sone <sthaya@gmail.com>
2020-10-25 19:52:13 -0700

    styled create product component

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProductsComponent } from './products.component';
-import { CreateProductComponent} from './create-product/create-product.component'
+import { CreateProductComponent } from './create-product/create-product.component';
 import { ViewProductComponent } from './view-product/view-product.component';
 import { ViewAllProductsComponent } from './view-all-products/view-all-products.component';
 
