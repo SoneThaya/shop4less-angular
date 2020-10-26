4bb1e40cca7e00d5888e3b12e2231058bcdbdb07
Sone <sthaya@gmail.com>
2020-10-25 19:28:43 -0700

    styled header footer and sidebar

--- src/app/site-framework/site-framework.module.ts
+++ src/app/site-framework/site-framework.module.ts
@@ -4,12 +4,9 @@ import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 
-
-
 @NgModule({
   declarations: [HeaderComponent, SidebarComponent, FooterComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [HeaderComponent, SidebarComponent, FooterComponent],
 })
-export class SiteFrameworkModule { }
+export class SiteFrameworkModule {}
