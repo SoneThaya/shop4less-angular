eabaa6dee88761e549caa8f066ad17cb126eaa70
Sone <sthaya@gmail.com>
2020-10-26 19:27:59 -0700

    setup routing

--- src/app/products/view-product/view-product.component.ts
+++ src/app/products/view-product/view-product.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-product',
   templateUrl: './view-product.component.html',
-  styleUrls: ['./view-product.component.css']
+  styleUrls: ['./view-product.component.css'],
 })
 export class ViewProductComponent implements OnInit {
+  constructor(private activatedRoute: ActivatedRoute) {}
 
-  constructor() { }
+  productId = 0;
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((data) => {
+      this.productId = data.id;
+    });
   }
-
 }
