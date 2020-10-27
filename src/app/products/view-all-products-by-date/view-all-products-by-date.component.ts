79cd9879c9b7b4582b9bec55fa2c0a5ff13c7e20
Sone <sthaya@gmail.com>
2020-10-26 20:54:47 -0700

    setup routes for delete, and views

--- src/app/products/view-all-products-by-date/view-all-products-by-date.component.ts
+++ src/app/products/view-all-products-by-date/view-all-products-by-date.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-all-products-by-date',
   templateUrl: './view-all-products-by-date.component.html',
-  styleUrls: ['./view-all-products-by-date.component.css']
+  styleUrls: ['./view-all-products-by-date.component.css'],
 })
 export class ViewAllProductsByDateComponent implements OnInit {
+  searchDate = '';
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe((data) => {
+      this.searchDate = data.date;
+    });
   }
-
 }
