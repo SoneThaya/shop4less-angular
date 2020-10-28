dfee4a7db12a323c2e1a9627f68f0e3181ebdae5
Sone <sthaya@gmail.com>
2020-10-27 19:47:29 -0700

    setup interface

--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Product } from './product';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,39 +9,39 @@ import { HttpClient } from '@angular/common/http';
 export class ProductsService {
   constructor(private httpClient: HttpClient) {}
 
-  createProduct(productBody) {
+  createProduct(productBody): Observable<Product> {
     const productUrl = 'http://localhost:3000/products';
 
-    return this.httpClient.post(productUrl, productBody); // return an observable
+    return this.httpClient.post<Product>(productUrl, productBody); // return an observable
   }
 
-  viewProduct(productId) {
+  viewProduct(productId): Observable<Product> {
     const productUrl = 'http://localhost:3000/product/' + productId;
 
-    return this.httpClient.get(productUrl); // return an observable
+    return this.httpClient.get<Product>(productUrl); // return an observable
   }
 
-  updateProduct(productId, productBody) {
+  updateProduct(productId, productBody): Observable<Product> {
     const productUrl = 'http://localhost:3000/product/' + productId;
 
-    return this.httpClient.put(productUrl, productBody); // return an observable
+    return this.httpClient.put<Product>(productUrl, productBody); // return an observable
   }
 
-  deleteProduct(productId) {
+  deleteProduct(productId): Observable<Product> {
     const productUrl = 'http://localhost:3000/product/' + productId;
 
-    return this.httpClient.delete(productUrl); // return an observable
+    return this.httpClient.delete<Product>(productUrl); // return an observable
   }
 
-  searchCategoryProducts(categoryId) {
+  searchCategoryProducts(categoryId): Observable<Product> {
     const productUrl = 'http://localhost:3000/product/category=' + categoryId;
 
-    return this.httpClient.get(productUrl); // return an observable
+    return this.httpClient.get<Product>(productUrl); // return an observable
   }
 
-  searchDateProducts(dateParam) {
+  searchDateProducts(dateParam): Observable<Product> {
     const productUrl = 'http://localhost:3000/product/date=' + dateParam;
 
-    return this.httpClient.get(productUrl); // return an observable
+    return this.httpClient.get<Product>(productUrl); // return an observable
   }
 }
